75a0447cdbc89747710a055a2be6044e17b9510b
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-17 10:43:13 +0200

    Add socialDbContext contructor

--- DataAccess/SocialDbContext.cs
+++ DataAccess/SocialDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class SocialDbContext
+    public class SocialDbContext:DbContext
     {
+
+        public SocialDbContext( DbContextOptions opt): base(opt) 
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+
     }
 }
