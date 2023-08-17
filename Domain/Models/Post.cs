6c3de625827ebe9438ceca191a3621920c59d4c2
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-17 10:38:50 +0200

    add ef and socialDbContext class

--- Domain/Models/Post.cs
+++ Domain/Models/Post.cs
@@ -8,5 +8,9 @@ namespace Domain.Models
 {
     public class Post
     {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastModifier { get; set; }
     }
 }
