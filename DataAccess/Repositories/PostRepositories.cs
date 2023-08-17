3d31cdfba2b1dc7b488fc5b18f17b8da5cc300ec
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-17 10:57:00 +0200

    add class to implement IPostRepository

---
+++ DataAccess/Repositories/PostRepositories.cs
@@ -0,0 +1,38 @@
+﻿using Application.Abstractions;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class PostRepositories : IPostRepository
+    {
+        public Task<Post> CreatePost(Post toCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePost(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Post>> GetAllPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPostById(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> UpdatePost(string updatedContent, int postId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
