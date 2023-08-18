beeeb2a4d1de8925718fe49f5044d0907ed998a8
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-18 11:31:59 +0200

    add connectionString in appsettings.json and configuration in program.cs

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,12 @@
+using Application.Abstractions;
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var cs = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(cs));
+builder.Services.AddScoped<IPostRepository, IPostRepository>();
 
 
 var app = builder.Build();
