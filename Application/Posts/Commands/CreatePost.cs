29507b62f9d5d79c39a454303af4f5294c081262
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-19 11:58:49 +0200

    add class createPost handler in commandHandler and DI IPostRepository in the constructor

--- Application/Posts/Commands/CreatePost.cs
+++ Application/Posts/Commands/CreatePost.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Models;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,10 @@ using System.Threading.Tasks;
 
 namespace Application.Posts.Commands
 {
-    public class CreatePost
+    public class CreatePost: IRequest<Post>
     {
+        public string? PostContent { get; set; }
+
+
     }
 }
