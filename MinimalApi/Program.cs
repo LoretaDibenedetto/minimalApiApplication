047e4898f91cb614be1c4cddb3954031616dc094
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-20 10:31:36 +0200

    add builder services to create post

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,6 +1,8 @@
 using Application.Abstractions;
+using Application.Posts.Commands;
 using DataAccess;
 using DataAccess.Repositories;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 var cs = builder.Configuration.GetConnectionString("Default");
 builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(cs));
 builder.Services.AddScoped<IPostRepository, PostRepositories>();
+builder.Services.AddMediatR(typeof(CreatePost));
+
 
 
 var app = builder.Build();
