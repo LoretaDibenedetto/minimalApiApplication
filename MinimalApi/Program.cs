b31ecaecbf254e4c273566dfe269caa943b120a8
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-22 12:38:22 +0200

    add registerEndpointDefinition in program.cs

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -19,42 +19,7 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/posts/{id}",async(IMediator mediator, int id) =>
-{
-    var getPost = new GetPostById { PostId = id };
-    var post = await mediator.Send(getPost);
-    return Results.Ok(post);
-
-})
-    .WithName("GetPostById");
-
-app.MapPost("api/posts",async (IMediator mediator, [FromBody]Post post ) =>
-{
-    var createPost = new CreatePost { PostContent = post.Content };
-    var createdPost = await mediator.Send(createPost);
-    return Results.CreatedAtRoute("GetPostById",new {createdPost.Id}, createdPost);
-});
-
-app.MapGet("/api/posts", async (IMediator mediator) =>
-{
-    var getCommand = new GetAllPosts();
-    var posts = await mediator.Send(getCommand);
-    return Results.Ok(posts);
-});
-
-app.MapPut("/api/posts/{id}", async (IMediator mediator, Post post, int id) =>
-{
-    var updatePost = new UpdatePost{ PostId = id, PostContent = post.Content };
-    var updatedPost = await mediator.Send(updatePost);
-    return Results.Ok(updatedPost);
-});
-
-app.MapDelete("/api/posts/{id}", async (IMediator mediator, int id) =>
-{
-    var deletePost = new DeletePost { PostId = id };
-    await mediator.Send(deletePost);
-    return Results.NoContent();
-});
+app.RegisterEndpointDefinition();
 
 
 app.Run();
