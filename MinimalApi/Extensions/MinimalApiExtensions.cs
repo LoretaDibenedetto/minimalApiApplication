5244a413562eb86f446483132462c546e2fc1582
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-22 11:51:05 +0200

    add RegisterEndpointDefinition MInimalApiExtensions

--- MinimalApi/Extensions/MinimalApiExtensions.cs
+++ MinimalApi/Extensions/MinimalApiExtensions.cs
@@ -5,6 +5,7 @@ using DataAccess;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
+using MinimalApi.Abstractions;
 
 namespace MinimalApi.Extensions
 {
@@ -17,5 +18,20 @@ namespace MinimalApi.Extensions
             builder.Services.AddScoped<IPostRepository, PostRepositories>();
             builder.Services.AddMediatR(typeof(CreatePost));
         }
+
+        public static void RegisterEndpointDefinition (this WebApplication app)
+        {
+            var endpointDefinition = typeof(Program).Assembly
+                .GetTypes()
+                .Where(t => t.IsAssignableTo(typeof(IEndpointDefinition)) && !t.IsAbstract && !t.IsInterface)
+                .Select(Activator.CreateInstance)
+                .Cast<IEndpointDefinition>();
+
+            foreach(var endpointDef in endpointDefinition) 
+            {
+                endpointDef.RegisterEndpoints(app); 
+            }
+        }
+
     }
 }
