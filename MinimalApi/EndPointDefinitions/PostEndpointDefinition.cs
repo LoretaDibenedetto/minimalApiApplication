3575fa51cf0ba7767e7915224734b09687e4992c
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-22 11:44:07 +0200

    add endpoint in postendpointdefinition

--- MinimalApi/EndPointDefinitions/PostEndpointDefinition.cs
+++ MinimalApi/EndPointDefinitions/PostEndpointDefinition.cs
@@ -1,4 +1,9 @@
-﻿using MinimalApi.Abstractions;
+﻿using Application.Posts.Commands;
+using Application.Posts.Queries;
+using Domain.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MinimalApi.Abstractions;
 
 namespace MinimalApi.EndPointDefinitions
 {
@@ -6,7 +11,42 @@ namespace MinimalApi.EndPointDefinitions
     {
         public void RegisterEndpoints(WebApplication app)
         {
-            throw new NotImplementedException();
+            app.MapGet("/api/posts/{id}", async (IMediator mediator, int id) =>
+            {
+                var getPost = new GetPostById { PostId = id };
+                var post = await mediator.Send(getPost);
+                return Results.Ok(post);
+
+            })
+            .WithName("GetPostById");
+
+            app.MapPost("api/posts", async (IMediator mediator, [FromBody] Post post) =>
+            {
+                var createPost = new CreatePost { PostContent = post.Content };
+                var createdPost = await mediator.Send(createPost);
+                return Results.CreatedAtRoute("GetPostById", new { createdPost.Id }, createdPost);
+            });
+
+            app.MapGet("/api/posts", async (IMediator mediator) =>
+            {
+                var getCommand = new GetAllPosts();
+                var posts = await mediator.Send(getCommand);
+                return Results.Ok(posts);
+            });
+
+            app.MapPut("/api/posts/{id}", async (IMediator mediator, Post post, int id) =>
+            {
+                var updatePost = new UpdatePost { PostId = id, PostContent = post.Content };
+                var updatedPost = await mediator.Send(updatePost);
+                return Results.Ok(updatedPost);
+            });
+
+            app.MapDelete("/api/posts/{id}", async (IMediator mediator, int id) =>
+            {
+                var deletePost = new DeletePost { PostId = id };
+                await mediator.Send(deletePost);
+                return Results.NoContent();
+            });
         }
     }
 }
