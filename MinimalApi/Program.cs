e248b173e3095519d35bc2490350aea8d3c05a1b
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-24 10:43:10 +0200

    add global exeption error

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -16,7 +16,19 @@ builder.RegisterServices();
 var app = builder.Build();
 
 
+app.Use(async (ctx, next) =>
+{
+    try
+    {
+        await next();
 
+    }
+    catch (Exception)
+    {
+        ctx.Response.StatusCode = 500;
+        await ctx.Response.WriteAsync("An error ocurred");
+    }
+});
 app.UseHttpsRedirection();
 
 app.RegisterEndpointDefinition();
